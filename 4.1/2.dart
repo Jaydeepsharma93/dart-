b13851a34c591e728f17562d7a38917245682262
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2024-01-18 15:06:33 +0530

    Update 2.dart

--- 4.1/2.dart
+++ 4.1/2.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 void main() {
   List name = [];
   stdout.write('Enter the size of list : ');
-  String? size = stdin.readLineSync();
-  int Size = int.parse(size!);
-
+  int Size = int.parse(stdin.readLineSync()!);
+  for (int i = 0; i < Size; i++) {
+    stdout.write('$i Enter name : ');
+    name.add(stdin.readLineSync());
+  }
   print(l_list(name));
 }
 
