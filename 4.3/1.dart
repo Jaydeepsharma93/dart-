89483aaa57d5fba07d9aafeb3e3d6e57eba37a32
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2024-01-22 15:16:57 +0530

    Add files via upload

---
+++ 4.3/1.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+class ques {
+  late int feet;
+  late int inches;
+  void set() {
+    stdout.write('Enter the feet : ');
+    feet = int.parse(stdin.readLineSync()!);
+    stdout.write('Enter the inches : ');
+    inches = int.parse(stdin.readLineSync()!);
+  }
+
+  void get() {
+    print("feet = ${feet + inches / 12}");
+    print("Inches = ${inches % 12}");
+  }
+}
+
+void main() {
+  ques q1 = ques();
+  q1.set();
+  q1.get();
+}
